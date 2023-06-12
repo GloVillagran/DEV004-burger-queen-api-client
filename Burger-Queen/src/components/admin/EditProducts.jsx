4e2544354fec9fa09cc63e3b151f3d02a4e82401
Gloria Villagran <glo.villagran@gmail.com>
2023-06-12 10:34:41 -0400

    Funcionalidades listas de todas las H.U

--- Burger-Queen/src/components/admin/EditProducts.jsx
+++ Burger-Queen/src/components/admin/EditProducts.jsx
@@ -5,20 +5,21 @@ import ReactModal from 'react-modal';
 no accedan al contenido principal cuando el modal está abierto. */
 ReactModal.setAppElement('#root');
 
-const EditWorker = ({ isOpen, worker, closeModal, updateWorker }) => {
+const EditProducts = ({ isOpen, product, closeModal, updateProduct }) => {
   const [editedData, setEditedData] = useState({
     id: '',
-    email: '',
-    role: '',
-    password: '',
+    name: '',
+    price: '',
+    image: '',
+    type: '',
     // Agrega aquí otros campos que desees editar
   });
 
   useEffect(() => {
-    if (worker) {
-      setEditedData(worker);
+    if (product) {
+      setEditedData(product);
     }
-  }, [worker]);
+  }, [product]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -30,14 +31,13 @@ const EditWorker = ({ isOpen, worker, closeModal, updateWorker }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    updateWorker(worker.id, editedData); // Llama a updateWorker con el ID del trabajador y los datos actualizados
+    updateProduct(product.id, editedData); // Llama a updateProduct con el ID del trabajador y los datos actualizados
     closeModal();
   };
 
   const handleCancel = () => {
     closeModal();
   };
-  
   
 
   return (
@@ -50,27 +50,34 @@ const EditWorker = ({ isOpen, worker, closeModal, updateWorker }) => {
           value={editedData.id}
           onChange={handleChange}
         />
-        <label>Email:</label>
+        <label>Name:</label>
         <input
           type="text"
-          name="email"
-          value={editedData.email}
+          name="name"
+          value={editedData.name}
           onChange={handleChange}
         />
-        <label>Password:</label>
+        <label>Price:</label>
         <input
-          type="password"
-          name="password"
-          value={editedData.password}
+          type="text"
+          name="price"
+          value={editedData.price}
           onChange={handleChange}
-          autoComplete="current-password"
         />
-       <label>Rol:</label>
-        <select name="role" value={editedData.role} onChange={handleChange}>
-          <option value="admin">Admin</option>
-          <option value="chef">Chef</option>
-          <option value="waiter">Waiter</option>
+        <label>Image:</label>
+        <input
+         type="text"
+         name="image"
+         value={editedData.image}
+         onChange={handleChange}
+        />
+       <label>Type:</label>
+       <select name="type" value={editedData.type} onChange={handleChange}>
+          <option value="vacio"></option>
+          <option value="breakfast">Breakfast</option>
+          <option value="lunch">Lunch</option>
         </select>
+        
         {/* Agrega aquí otros campos del formulario */}
         <button type="submit">Update</button>
         <button type="button" onClick={handleCancel}>Cancel</button>
@@ -79,4 +86,4 @@ const EditWorker = ({ isOpen, worker, closeModal, updateWorker }) => {
   );
 };
 
-export default EditWorker;
+export default EditProducts;
--- Burger-Queen/src/components/admin/ProductsList.jsx
+++ Burger-Queen/src/components/admin/ProductsList.jsx
@@ -2,14 +2,21 @@ import { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
 import MenuVerticalAdmin from './MenuVerticalAdmin';
 import { AuthContext } from '../../AuthContext';
+import EditProducts from './EditProducts';
 
 const ProductsList = () => {
     const { token } = useContext(AuthContext);
     const [products, setProducts] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [editedProduct, setEditedProduct] = useState(null);
+
 
     useEffect(() => {
+        fetchProducts();
+      }, [token]);
+    
         // Hacer la solicitud a la API simulada
-        const fetchProducts = async () => {
+    const fetchProducts = async () => {
         try {
             const response = await axios.get('http://localhost:8080/products', {
                 headers: {
@@ -21,35 +28,92 @@ const ProductsList = () => {
         } catch (error) {
             console.error('Error getting workers:', error);
         }
-    }; fetchProducts();
-    }, [token]);
-
-    const handleEdit = (productId) => {
-        // Lógica para editar el producto con el ID dado
-        console.log(`Editar producto con ID: ${productId}`);
     };
 
-    const handleDelete = (productId) => {
-        // Lógica para eliminar el producto con el ID dado
-        console.log(`Eliminar producto con ID: ${productId}`);
+    const editProducts = async (productId) => {
+        setIsModalOpen(true);
+        try {
+            const response = await axios.get(`http://localhost:8080/products/${productId}`, {
+              headers: {
+                Authorization: `Bearer ${token}`
+              }
+            });
+            setEditedProduct(response.data);
+        } catch (error) {
+                console.error('Error getting workers:', error);
+              }
+       /*  // Lógica para editar el producto con el ID dado
+        console.log(`Editar producto con ID: ${productId}`); */
     };
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setEditedProduct(null);
+      };
 
+      const updateProduct = async (productId, updatedData) => {
+        try {
+          const response = await axios.patch(`http://localhost:8080/products/${productId}`, 
+          updatedData,
+          {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+          closeModal();
+          // Actualiza los datos de los trabajadores
+          fetchProducts(response.data);
+        } catch (error) {
+          console.error('Failed to update worker:', error);
+        }
+      };
+      
+    const deleteProduct = async (productId) => {
+        try {
+          const response = await axios.delete(`http://localhost:8080/products/${productId}`, {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+          fetchProducts(response.data);
+        } catch (error) {
+          console.error('Error deleting product:', error);
+        }
+      };
+    const renderProducts = () => {
+        if (products.length === 0) {
+          return <p>No products available.</p>;
+        }
     return (
         <div>
             <MenuVerticalAdmin />
-            <h1>Lista de productos</h1>
+           
             {products && products.map(product => (
                 <div key={product.id}>
                     <p>{product.name}</p>
-                    <p>{product.price}</p>
+                    <p>$:{product.price}</p>
                     <img src={product.image} alt={product.name} />
                     <p>{product.type}</p>
-                    <button onClick={() => handleEdit(product.id)}>Editar</button>
-                    <button onClick={() => handleDelete(product.id)}>Eliminar</button>
+                    <button onClick={() => editProducts(product.id)}>Edit</button>
+                    <button onClick={() => deleteProduct(product.id)}>Delete</button>
                 </div>
             ))}
+            
+      <EditProducts
+      isOpen={isModalOpen}
+      product={editedProduct}
+      closeModal={closeModal}
+      updateProduct={updateProduct}
+    />
         </div>
     );
+};
+
+return (
+    <div>
+      <h3>List Products</h3>
+      {renderProducts()}
+    </div>
+  );
 };
 
 export default ProductsList;
